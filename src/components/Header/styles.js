e2f45b7c6ed85b25946e559c39f7664eac89f392
Eduardo Dias <103222318+eduardoazvdias@users.noreply.github.com>
2022-06-27 17:02:51 -0300

    Update styles.js

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
 height: 150px;
 text-align: center;
-background: black;
+background: teal;
 `;
 
 export const Header = styled.h1``;
